f1aa6ce83cad09740beb5677acc4bfefa864b521
vishal <vishal.gaur@uah.edu>
2023-07-24 10:25:52 -0500

    Added the transformation scripts for CO2 flux and SEDAC gridded population data.

---
+++ cog_transformation_scripts/nasa_ceos_co2_flux_data_transformation.py
@@ -0,0 +1,76 @@
+import os
+import xarray
+import re
+import pandas as pd
+import json
+import tempfile
+import boto3
+import rasterio
+
+session = boto3.session.Session()
+s3_client = session.client("s3")
+bucket_name = "ghgc-data-store-dev"
+date_fmt = "%Y%m"
+year_ = 2015
+
+COG_PROFILE = {"driver": "COG", "compress": "DEFLATE"}
+
+files_processed = pd.DataFrame(columns=["file_name", "COGs_created"])
+for name in os.listdir("new_data"):
+    ds = xarray.open_dataset(
+        f"new_data/{name}",
+        engine="netcdf4",
+    )
+    ds = ds.rename({"latitude": "lat", "longitude": "lon"})
+    # assign coords from dimensions
+    ds = ds.assign_coords(lon=(((ds.lon + 180) % 360) - 180)).sortby("lon")
+    ds = ds.assign_coords(lat=list(ds.lat))
+
+    variable = [var for var in ds.data_vars]
+
+    for time_increment in range(0, len(ds.year)):
+        for var in variable[2:]:
+            filename = name.split("/ ")[-1]
+            filename_elements = re.split("[_ .]", filename)
+            try:
+                data = ds[var].sel(year=time_increment)
+            except KeyError:
+                data = ds[var]
+
+            data = data.reindex(lat=list(reversed(data.lat)))
+
+            data.rio.set_spatial_dims("lon", "lat")
+            data.rio.write_crs("epsg:4326", inplace=True)
+
+            # generate COG
+            COG_PROFILE = {"driver": "COG", "compress": "DEFLATE"}
+
+            date = str(year_ + time_increment)
+            filename_elements.insert(-1, date)
+            # # insert date of generated COG into filename
+            filename_elements.pop()
+            filename_elements[-1] = date
+            filename_elements.insert(2, var)
+            cog_filename = "_".join(filename_elements)
+            # # add extension
+            cog_filename = f"{cog_filename}.tif"
+
+            with tempfile.NamedTemporaryFile() as temp_file:
+                data.rio.to_raster(temp_file.name, **COG_PROFILE)
+                s3_client.upload_file(
+                    Filename=temp_file.name,
+                    Bucket=bucket_name,
+                    Key=f"ceos_co2_flux/{cog_filename}",
+                )
+
+            files_processed = files_processed._append(
+                {"file_name": name, "COGs_created": cog_filename},
+                ignore_index=True,
+            )
+
+            print(f"Generated and saved COG: {cog_filename}")
+
+files_processed.to_csv(
+    f"s3://{bucket_name}/ceos_co2_flux/files_converted.csv",
+)
+print("Done generating COGs")
