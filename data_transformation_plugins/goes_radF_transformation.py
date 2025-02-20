0361cd568708fa5b6afa229499623f7c56fcb8c7
Paridhi Parajuli <46867571+paridhi-parajuli@users.noreply.github.com>
2025-02-20 14:02:38 -0600

    change date extraction

--- data_transformation_plugins/goes_radF_transformation.py
+++ data_transformation_plugins/goes_radF_transformation.py
@@ -8,6 +8,7 @@ import numpy as np
 from typing import Dict
 from rasterio.enums import Resampling
 from rasterio.io import MemoryFile
+from datetime import datetime, timedelta
 
 # Define AOI as a shapely Polygon
 aoi_coords = [[-102.8148701375, 6.1943456775], [-13.3448605043, 6.1943456775], 
@@ -41,8 +42,25 @@ def goes_radF_transformation(file_obj, name: str, nodata: int) -> Dict[str, xr.D
         time_parts = time_coverage_start.split("T")
         formatted_time = f"{time_parts[0]}T{time_parts[1].replace('.', '')}"[:19]
 
+        
+
         # Create the output filename
-        band_name = name.split("_G16")[0][-5:]  # Split before "G16" and take band
+        band_name = name.split("_G16_s")[0][-5:]  # Split before "G16" and take band
+        timestamp = name.split("_G16_s")[-1][:11]
+        
+        year = int(timestamp[:4])      # 2024
+        doy = int(timestamp[4:7])      # 181
+        hour = int(timestamp[7:9])     # 06
+        minute = int(timestamp[9:11])  # 00
+        second = 0
+        
+        # Convert DOY to MM-DD
+        date = datetime(year, 1, 1) + timedelta(days=doy - 1)
+        formatted_date = date.strftime("%Y-%m-%d")
+        
+        # Format the final timestamp
+        iso_timestamp = f"{formatted_date}T{hour:02d}:{minute:02d}:{second:02d}Z"
+
         cog_filename = f"G16ABI_{band_name}_{formatted_time}Z.tif"
         
         da = goes_to_wgs(ds, variable_name="Rad")
