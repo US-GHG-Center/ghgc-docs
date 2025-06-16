1840c06fa48afc22246d1118fa8382638173cbfc
Paridhi <073bct525.paridhi@pcampus.edu.np>
2025-06-16 14:39:59 -0500

    add rasterio engine

--- data_transformation_plugins/odiac_ffco2_monthgrid_v2024_transformation.py
+++ data_transformation_plugins/odiac_ffco2_monthgrid_v2024_transformation.py
@@ -5,7 +5,7 @@ from typing import Dict
 def odiac_ffco2_monthgrid_v2024_transformation(file_obj, name: str, nodata: int) -> Dict[str, xr.DataArray]:
     """Process a single ODIAC tif file from S3File object and return transformed DataArray."""
     var_data_netcdf = {}
-    with xr.open_dataarray(file_obj) as ds:
+    with xr.open_dataarray(file_obj,engine="rasterio") as ds:
         xds = xds.where(xds!=0, -9999)
         xds.rio.set_spatial_dims("x", "y", inplace=True)
         xds.rio.write_nodata(-9999, inplace=True)
