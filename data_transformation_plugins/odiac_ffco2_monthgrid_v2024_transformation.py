ee552b42258d8d694295cd138110d1cd7e7949b7
Paridhi <073bct525.paridhi@pcampus.edu.np>
2025-06-16 14:52:27 -0500

    change var name

--- data_transformation_plugins/odiac_ffco2_monthgrid_v2024_transformation.py
+++ data_transformation_plugins/odiac_ffco2_monthgrid_v2024_transformation.py
@@ -6,12 +6,12 @@ def odiac_ffco2_monthgrid_v2024_transformation(file_obj, name: str, nodata: int)
     """Process a single ODIAC tif file from S3File object and return transformed DataArray."""
     var_data_netcdf = {}
     with xr.open_dataarray(file_obj,engine="rasterio") as ds:
-        xds = xds.where(xds!=0, -9999)
-        xds.rio.set_spatial_dims("x", "y", inplace=True)
-        xds.rio.write_nodata(-9999, inplace=True)
-        xds.rio.write_crs("epsg:4326", inplace=True)
+        ds = ds.where(ds!=0, -9999)
+        ds.rio.set_spatial_dims("x", "y", inplace=True)
+        ds.rio.write_nodata(-9999, inplace=True)
+        ds.rio.write_crs("epsg:4326", inplace=True)
 
-        var_data_netcdf[name] = xds
+        var_data_netcdf[name] = ds
 
         return var_data_netcdf
 
